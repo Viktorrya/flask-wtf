c2aa1fd985f35b0833e47a6d556259bb1438c4e1
Виктория <vika_nazarova_05@inbox.ru>
2021-04-21 18:31:05 +0300

    Тренировки в полёте

--- main.py
+++ main.py
@@ -10,5 +10,16 @@ def index(title):
     return render_template('base.html', **param)
 
 
+@app.route('/training/<prof>')
+def training_prof(prof):
+    return render_template('training.html', prof=prof)
+
+
+@app.route('/list_prof/<param_list>')
+def list_prof(param_list):
+    profs = ['Инженер-исследователь', 'Пилот', 'Врач']
+    return render_template('list_prof.html', param_list=param_list, profs=profs)
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
